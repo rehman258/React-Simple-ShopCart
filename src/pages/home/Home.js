d17875e0c9d9f30f88a85a571d764416a37d2784
rehman258 <rehman1998eliyev@gmail.com>
2022-12-12 06:16:41 +0400

    working on home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,7 +4,8 @@ import defaultTheme from '../../Theme/Theme';
 import {createTheme,ThemeProvider} from '@mui/material/styles';
 import {
     Box,
-    Grid
+    Grid,
+    Typography
 } from '@mui/material';
 
 import {connect} from 'react-redux';
@@ -17,26 +18,116 @@ import Logo from '../../assets/img/shop-logo.png'
 
 
 const Home = ({homeReducer}) => {
+
+
+    const homeTheme = createTheme({
+        components:{
+            MuiTypography:{
+                styleOverrides:{
+                    root:{
+                        '&.aboutContent':{
+                            color:'white',
+                            padding:'50px 100px'
+                        },
+                        '&.section-header':{
+                            width:'100%',
+                            fontSize:'56px',
+                            textAlign:'center',
+                            margin:'50px 0px',
+                            color:'#fff'
+                        }
+                    }
+                }
+            },
+            MuiBox:{
+                styleOverrides:{
+                    root:{
+                        '&.about-section"':{
+                            padding:'50px 0px'
+                        }
+                    }
+                }
+            },
+            MuiGrid:{
+                styleOverrides:{
+                    root:{
+                        '& img.home-about--section-logo':{
+                            padding:'0px 30px'
+                        },
+                        alignItems:'center',
+                        justifyContent:'center'
+                    }
+                }
+            },
+            ...defaultTheme.components,
+
+            
+        }
+    })
+
     return (
         <section>
-            <Box className="banner-slider">
-                <BannerSlider
-                    sliderList={homeReducer.sliderList}
-                />
-            </Box>
-            <Box className="about-section">
-                <Grid container >
-                    <Grid item xs={6}>
-                        <p> 
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae non alias temporibus in deserunt laudantium maiores ex voluptatum vel? Facere rerum, nisi doloremque repudiandae odit culpa totam sit! Aspernatur, consequuntur?
-                        </p>
+            <ThemeProvider theme={homeTheme}>
+
+                <Box className="banner-slider">
+                    <BannerSlider
+                        sliderList={homeReducer.sliderList}
+                        />
+                </Box>
+                <div className="line-effect"></div>
+                <Box 
+                    sx={{
+                        padding:'50px 0px'
+                    }}
+                >
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <Typography className='aboutContent'> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae non alias temporibus in deserunt laudantium maiores ex voluptatum vel? Facere rerum, nisi doloremque repudiandae odit culpa totam sit! Aspernatur, consequuntur?
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae non alias temporibus in deserunt laudantium maiores ex voluptatum vel? Facere rerum, nisi doloremque repudiandae odit culpa totam sit! Aspernatur, consequuntur?
+                            </Typography>
+                        </Grid>
+                        <Grid 
+                            item
+                            xs={6}
+                            sx={{
+                                display:'flex',
+                                justifyContent:'center',
+                                alignItems:'center'
+                            }} 
+                        >
+                            <img className='home-about--section-logo' src={Logo} alt="" />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <img src={Logo} alt="" />
+                </Box>
+                <div className="line-effect"></div>
+                <Typography className="section-header">
+                    Products
+                </Typography>
+                <Box
+                    className="product-list"
+                    sx={{
+                        padding:'50px 0px'
+                    }}
+                >
+                    <Grid container
+                        
+                    >
+                        <Grid item xs={3}>
+                            1
+                        </Grid>
+                        <Grid item xs={3}>
+                            2
+                        </Grid>
+                        <Grid item xs={3}>
+                            3
+                        </Grid>
+                        <Grid item xs={3}>
+                            4
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Box>
-            <div className="line-effect"></div>
+                </Box>
+            </ThemeProvider>
         </section>
     );
 }
